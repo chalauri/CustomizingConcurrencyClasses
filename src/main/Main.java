101276e349276c37054817e98f5a7f9c9dd74142
G. Chalauri <g.chalauri@mia.gov.ge>
2017-03-30 07:33:20 +0400

    implementing priority based executor class

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,11 @@ package main;
 
 import main.customizing_threadPoolExecutor_class.MyExecutor;
 import main.customizing_threadPoolExecutor_class.SleepTwoSecondsTask;
+import main.implementing_priority_based_executor_class.MyPriorityTask;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Created by G.Chalauri on 3/30/2017.
@@ -16,7 +14,39 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
-        customizeThreadPoolExecutorClassExample();
+        // customizeThreadPoolExecutorClassExample();
+        implementingPriorityBasedExecutorExample();
+    }
+
+    private static void implementingPriorityBasedExecutorExample() {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 2, 1,
+                TimeUnit.SECONDS, new PriorityBlockingQueue<Runnable>());
+
+        for (int i = 0; i < 4; i++) {
+            MyPriorityTask task = new MyPriorityTask("Task " + i, i);
+            executor.execute(task);
+        }
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 4; i < 8; i++) {
+            MyPriorityTask task = new MyPriorityTask("Task " + i, i);
+            executor.execute(task);
+        }
+
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("Main: End of the program.\n");
     }
 
     private static void customizeThreadPoolExecutorClassExample() {
